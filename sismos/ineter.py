cdd193374867ef9556c62507ee90fc25f40c5a32
Igor Guerrero <igorgue@protonmail.com>
2022-12-04 18:20:18 -0600

    Add initial test we get a dict now, we are ready to get last sismos, well kinda

--- sismos/ineter.py
+++ sismos/ineter.py
@@ -6,11 +6,10 @@ This is the API for the INETER's "API".
 import httpx
 from bs4 import BeautifulSoup
 
-
 DATA_URL = "https://webserver2.ineter.gob.ni/geofisica/sis/events/sismos.php"
 
 
-def get_lines_from_api() -> list[str]:
+def get_lines_from_api() -> list[dict]:
     """
     Get the lines from the INETER's "API".
     """
@@ -19,14 +18,38 @@ def get_lines_from_api() -> list[str]:
     return parse_html(content)
 
 
-def parse_html(content: str) -> list[str]:
+def parse_html(content: str) -> list[dict]:
     """
     Get the lines from the INETER's "API".
     """
     soup = BeautifulSoup(content, "html.parser")
     pres = list(soup.find_all("pre"))
 
-    return [pre.text for pre in pres]
+    return [_parse_pre(pre.text) for pre in pres]
+
+
+def _parse_pre(pre: str) -> dict:
+    """
+    Parse the pre tag.
+    """
+    parts = pre.split()
+
+    local_time = " ".join(parts[0:2])
+    lat, long = parts[2:4]
+    depth = parts[4]
+    richter = parts[5]
+    description = parts[6]
+    location = " ".join(parts[7:])
+
+    return {
+        "datetime": local_time,
+        "lat": lat,
+        "long": long,
+        "depth": depth,
+        "richter": richter,
+        "description": description,
+        "location": location,
+    }
 
 
 def _fetch_data() -> str:
--- tests/test_ineter.py
+++ tests/test_ineter.py
@@ -3,26 +3,43 @@ test_ineter.py
 
 This is the test for the INETER's "API".
 """
+from os import path
+
 from sismos.ineter import parse_html
+
+BASE_DIR = path.dirname(path.abspath(__file__))
 
 
 def test_parse_html():
     """
     tests the function that parses the html content from ineter.
     """
-    content = _get_test_content()
+    content = _get_test_content("sismos.0.php.html")
+    items = parse_html(content)
 
-    data = parse_html(content)
+    assert len(items) == 147, "There should be 147 items"
 
-    assert data, "Data is empty"
+    first = items[0]
+
+    assert first["datetime"] == "22/12/04 16:06:29", "The datetime should be correct"
+    assert first["lat"] == "12.590", "The latitude should be correct"
+    assert first["long"] == "-90.132", "The longitude should be correct"
+    assert first["depth"] == "5", "The depth should be correct"
+    assert first["richter"] == "4.1", "The richter should be correct"
+    assert first["description"] == "C", "The description should be correct"
+    assert (
+        first["location"] == "116 Km al sur de Acajutla, El Salvador"
+    ), "The location should be correct"
 
 
-def _get_test_content() -> str:
+def _get_test_content(filename: str) -> str:
     """
     Get the test content.
     """
     content = ""
-    with open("tests/data/sismos.php.html", "r", encoding="utf-8") as file:
+    full_path = path.join(BASE_DIR, "data", filename)
+
+    with open(full_path, "r", encoding="utf-8") as file:
         content = file.read()
 
     assert content, "Content is empty"
