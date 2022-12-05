9cdab31fdce287f94c6171aeadd84c226212d74b
Igor Guerrero <igorgue@protonmail.com>
2022-12-04 20:58:52 -0600

    More fun code

--- sismos/bot.py
+++ sismos/bot.py
@@ -4,6 +4,7 @@ bot.py
 This is the bot for the webhooks of Twilio's WhatsApp API.
 """
 import os
+from datetime import datetime
 
 from dotenv import load_dotenv
 from sqlalchemy.orm import Session
@@ -27,19 +28,46 @@ def respond(db: Session, message: str) -> str:  # pylint: disable=invalid-name
     Respond to the message with the given content.
     """
     response = MessagingResponse()
-    usage_message = "Comandos: [ultimos|ayuda]"
     message = message.lower()
 
-    if message in ["ultimos", "últimos"]:
+    if _is_ultimos(message):
         response.message(_get_last_sismos(db))
-    elif message == "ayuda":
-        response.message(usage_message)
+    elif _is_ayuda(message):
+        response.message(_get_help())
     else:
         print(f"Unknown command: {message}")
 
-        response.message(usage_message)
+        response.message(_get_help())
 
     return response.to_xml()
+
+
+def _is_ultimos(message: str) -> bool:
+    """
+    Check if the message is for the last sismos.
+    """
+    conds = (
+        message in ["ultimos", "últimos"]
+        or message.startswith("ult")
+        or message.startswith("últ")
+        or message.startswith("lastest")
+        or message.startswith("last")
+    )
+
+    return conds
+
+
+def _is_ayuda(message: str) -> bool:
+    """
+    Check if the message is for the help.
+    """
+    conds = (
+        message in ["ayuda", "help"]
+        or message.startswith("ayu")
+        or message.startswith("hel")
+    )
+
+    return conds
 
 
 def _get_last_sismos(db: Session) -> str:  # pylint: disable=invalid-name
@@ -52,8 +80,118 @@ def _get_last_sismos(db: Session) -> str:  # pylint: disable=invalid-name
 
     content = ""
     for sismo in Sismo.latest(db):
-        content += f"{sismo.richter} - {sismo.location} - {sismo.country} - {sismo.created}\n"
+        country_emoji = country_to_flag_emoji(str(sismo.country))
+        country_abbr = country_to_abbr(str(sismo.country))
+        richter_emoji = richter_scale_to_emoji(str(sismo.richter))
+        time_ago = datetime_to_time_ago_in_spanish(sismo.created)  # type: ignore
+
+        content += (
+            f"{country_abbr} {country_emoji}: {sismo.richter} {richter_emoji}\n"
+            f"{sismo.location}. {time_ago}\n\n"
+        )
 
     sufix = "\n\nFuente: https://www.ineter.gob.ni/sismos/"
 
     return prefix + content + sufix
+
+
+def _get_help() -> str:
+    """
+    Get the help message.
+    """
+    return (
+        "Comandos: [ultimos|ayuda], escala:\n\n"
+        "🌋: 0.0 - 1.9\n"
+        "🌋🌋: 2.0 - 3.9\n"
+        "🌋🌋🌋: 4.0 - 5.9\n"
+        "🌋🌋🌋🌋: 6.0 - 6.9\n"
+        "🌋🌋🌋🌋🌋: 7.0 - ..."
+    )
+
+
+def country_to_flag_emoji(country: str) -> str:
+    """
+    Convert the country to the flag emoji.
+    """
+    assert country
+
+    data = {
+        "Nicaragua": "🇳🇮",
+        "Costa Rica": "🇨🇷",
+        "Panama": "🇵🇦",
+        "Honduras": "🇭🇳",
+        "El Salvador": "🇸🇻",
+        "Guatemala": "🇬🇹",
+        "Mexico": "🇲🇽",
+    }
+
+    return data.get(country, "")
+
+
+def country_to_abbr(country: str) -> str:
+    """
+    Convert the country to the abbreviation.
+    """
+    assert country
+
+    data = {
+        "Nicaragua": "NI",
+        "Costa Rica": "CR",
+        "Panama": "PA",
+        "Honduras": "HN",
+        "El Salvador": "SV",
+        "Guatemala": "GT",
+        "Mexico": "MX",
+    }
+
+    return data.get(country, "")
+
+
+def richter_scale_to_emoji(richter: str) -> str:
+    """
+    Convert the richter scale to the emoji.
+    """
+    ritcher = float(richter)
+
+    if ritcher <= 1.9:
+        return "🌋"
+    if ritcher <= 3.9:
+        return "🌋🌋"
+    if ritcher <= 5.9:
+        return "🌋🌋🌋"
+    if ritcher <= 6.9:
+        return "🌋🌋🌋🌋"
+
+    return "🌋🌋🌋🌋🌋"
+
+
+def datetime_to_time_ago_in_spanish(date: datetime) -> str:
+    """
+    Convert the datetime to the time ago in spanish.
+    """
+    assert date
+
+    now = datetime.now()
+    diff = now - date
+
+    if diff.days > 0:
+        if diff.days == 1:
+            return "hace 1 día"
+
+        return f"hace {diff.days} días"
+
+    hours = diff.seconds // 3600
+    if hours > 0:
+        if hours == 1:
+            return "hace 1 hora"
+
+        return f"hace {hours} horas"
+
+    minutes = diff.seconds // 60
+    if minutes > 0:
+        if minutes == 1:
+            return "hace 1 minuto"
+
+        return f"hace {minutes} minutos"
+
+    return "hace unos segundos"
