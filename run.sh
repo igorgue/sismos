bdc4a5e529a9589aa2a612094f2eaff3364b53e8
Igor Guerrero <igorgue@protonmail.com>
2023-01-10 17:26:09 -0600

    Fix cronjob on run.sh

--- run.sh
+++ run.sh
@@ -15,8 +15,11 @@ poetry run python sismos/fetch_initial_data.py
 
 # Add cronjob to fetch data every 5 minutes
 if [ -n "$IN_DOCKER" ]; then
-    echo '*/5 * * * * poetry run python sismos/fetch_initial_data.py' | crontab -
+    echo '*/5 * * * * poetry run python sismos/fetch_data.py' | crontab -
 fi
+
+# Add intial data
+poetry run python sismos/fetch_initial_data.py
 
 # services
 poetry run uvicorn sismos:app --host 0.0.0.0 --port 6200 $RELOAD &
